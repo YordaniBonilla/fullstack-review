99c6d862a28deaca5e39e52ee94e3941fa66a662
YordaniBonilla <b.yordani93@gmail.com>
2019-06-12 22:51:23 -0500

    cut and paste Router Tags inside Top level Divs

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link} from "react-router-dom";
-import CreateTodo from '../components/CreateTodo'
-import EditTodo from '../components/EditTodo'
-import TodosList from '../components/TodosList'
+import CreateTodo from './components/CreateTodo'
+import EditTodo from './components/EditTodo'
+import TodosList from './components/TodosList'
+
 function App() {
   return (
     <Router>
       <div className="container">
         <h2>Mern-Stack Todo App</h2>
+        <Route path="/" exact component={ TodosList }/>
+        <Route path="/edit/:id" component={ EditTodo }/>
+        <Route path="/create" component={ CreateTodo }/>
       </div>
-      <Router path="/" exact component={ TodosList }/>
-      <Router path="/edit/:id" component={ EditTodo }/>
-      <Router path="/create" component={ CreateTodo }/>
+
     </Router>
   );
 }
